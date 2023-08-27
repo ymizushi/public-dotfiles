9613910f1c904a159a530f68e899923432f30f75
ymizushi <mizushi@gmail.com>
2023-08-27 14:08:50 +0900

    add .gitconfig

--- symboliclink.bash
+++ symboliclink.bash
@@ -1,11 +1,28 @@
 #!/bin/bash
-DOT_FILES=( .zshrc .tmux.conf .tigrc .ctags  .agignore )
+
+remove_exists_files()
+{
+    if [ -e $HOME/$1 ];then
+        rm $HOME/$1;
+        echo "remove $HOME/$1"
+    fi
+}
+
+remove_exists_dir()
+{
+    if [ -e $HOME/$1 ];then
+        rm -r $HOME/$1;
+        echo "remove $HOME/$1"
+    fi
+}
+
+DOT_FILES=( .zshrc .tmux.conf .tigrc .ctags  .agignore .gitconfig)
 
 for file in ${DOT_FILES[@]}
 do
     remove_exists_files $file
     ln -s $HOME/dotfiles/$file $HOME/$file
-    echo "link $HOME/public-dotfiles/$file to $HOME/$file"
+    echo "link $HOME/dotfiles/$file to $HOME/$file"
 done
 
 echo "symbolic linking is done! ";
